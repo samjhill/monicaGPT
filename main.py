17e7b3275803e71aec0914ef69396b9859209971
Sam Hill <samhilll@Sams-MacBook-Pro.local>
2023-01-10 22:43:16 -0500

    cleaup

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 MONICA_ACCESS_TOKEN = os.getenv('monica_access_token')
 
-GPT_RESPONSE_MARKER = "\n \n RESPONSE FROM GPT: \n"
+GPT_RESPONSE_MARKER = "RESPONSE FROM GPT:"
 
 def get_calls():
     call = calls.Calls(MONICA_ACCESS_TOKEN)
@@ -24,21 +24,27 @@ def add_gpt_response_to_call(content, response, contact_id, called_at):
     return call.update_call(updated_call_log, contact_id, called_at)
 
 
-calls_data = get_calls()
+def manage_calls():
+    calls_data = get_calls()
 
-for call in calls_data:
-    call_content = call["content"]
+    for index, call in enumerate(calls_data):
+        call_content = call["content"]
 
-    if GPT_RESPONSE_MARKER in call_content:
-        print("skipping this call, as it already has a response")
-        continue
+        if GPT_RESPONSE_MARKER in call_content:
+            print("skipping this call, as it already has a response")
+            continue
 
-    contact_id = call["id"]
-    called_at = call["called_at"]
+        contact_id = call["id"]
+        called_at = call["called_at"]
 
-    gpt_response = get_conversation_suggestions(call_content)
-    print(gpt_response)
+        gpt_response = get_conversation_suggestions(call_content)
 
-    monica_response = add_gpt_response_to_call(call_content, gpt_response, contact_id, called_at)
+        monica_response = add_gpt_response_to_call(call_content, gpt_response, contact_id, called_at)
 
-    print(monica_response)
+        print(f"added GPT response to {contact_id}. {index + 1 / len(calls_data)}%")
+    
+    print("finished adding responses to calls")
+
+
+if __name__ == "__main__":
+  manage_calls()
